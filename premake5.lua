2925f9bab2eeeea9ac47af4d002d0cd9848de2dd
hackthedoc <hackthedoc@gmail.com>
2025-10-28 20:30:36 +0100

    added freetype and fixed premake

--- premake5.lua
+++ premake5.lua
@@ -76,4 +76,57 @@ project "freetype"
 		runtime "Release"
 		optimize "on"
 
-	filte
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
+
+project "msdfgen"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/obj/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"core/**.h",
+		"core/**.hpp",
+		"core/**.cpp",
+		"ext/**.h",
+		"ext/**.hpp",
+		"ext/**.cpp",
+		"lib/**.cpp",
+		"include/**.h"
+	}
+
+	includedirs {
+		"include",
+		"freetype/include"
+	}
+
+	defines {
+		"MSDFGEN_USE_CPP11"
+	}
+
+	links {
+		"freetype"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
+		
